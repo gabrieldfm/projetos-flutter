08c1f6d05778af554fe3d18e9fc490c33760effc
Carlos Gabriel <gabrieldfm13@gmail.com>
2020-06-17 20:22:54 -0300

    config mapa

--- minhas_viagens/lib/Mapa.dart
+++ minhas_viagens/lib/Mapa.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
 
 class Mapa extends StatefulWidget {
   @override
@@ -6,13 +8,32 @@ class Mapa extends StatefulWidget {
 }
 
 class _MapaState extends State<Mapa> {
+
+  Completer<GoogleMapController> _controller = Completer();
+
+  _onMapCreated(GoogleMapController controller){
+    _controller.complete(controller);
+  }
+
+  _exibirMarcador(LatLng latLng){
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Mapa"),
       ),
-      body: Container(),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(-23.55553, -23.55554),
+          zoom: 18
+        ),
+        onMapCreated: _onMapCreated,
+        onLongPress: _exibirMarcador,
+      ),
     );
   }
 }
