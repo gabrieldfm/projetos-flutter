e3068124408186b3019bc13a6487e5563649d27d
Carlos Gabriel <gabrieldfm13@gmail.com>
2020-06-16 22:58:46 -0300

    motagem da estrutura

--- minhas_viagens/lib/Home.dart
+++ minhas_viagens/lib/Home.dart
@@ -6,13 +6,68 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List _listaViagens = ["Natal", "ferias"];
+
+  _abrirMapa(){}
+
+  _excluirViagem(){}
+
+  _adicionarLocal(){}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Minhas viagens"),
       ),
-      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          _adicionarLocal();
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Color(0xff0066cc),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: _listaViagens.length,
+              itemBuilder: (context, index){
+                String titulo = _listaViagens[index];
+
+                return GestureDetector(
+                  onTap: (){
+                    _abrirMapa();
+                  },
+                  child: Card(
+                    child: ListTile(
+                      title: Text(titulo),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          GestureDetector(
+                            onTap: (){
+                              _excluirViagem();
+                            },
+                            child: Padding(
+                              padding: EdgeInsets.all(8),
+                              child: Icon(
+                                Icons.remove_circle,
+                                color: Colors.red,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
