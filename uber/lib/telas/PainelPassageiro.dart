14fc448030d11fe6101442cd51e518080a35926b
Carlos Gabriel <gabrieldfm13@gmail.com>
2020-06-28 19:04:53 -0300

    deslogando usuario

--- uber/lib/telas/PainelPassageiro.dart
+++ uber/lib/telas/PainelPassageiro.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class PainelPassageiro extends StatefulWidget {
@@ -6,11 +7,42 @@ class PainelPassageiro extends StatefulWidget {
 }
 
 class _PainelPassageiroState extends State<PainelPassageiro> {
+  List<String> itensMenu = ["Deslogar", "Configurações"];
+
+  _escolhaItemMenu(String escolha){
+    switch (escolha) {
+      case "Deslogar":
+        _deslogar();
+        break;
+      case "Configurações":
+        break;
+    }
+  }
+
+  _deslogar()async{
+    FirebaseAuth auth = FirebaseAuth.instance;
+    await auth.signOut();
+    Navigator.pushReplacementNamed(context, "/");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("painel passageiro"),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: _escolhaItemMenu,
+            itemBuilder: (context){
+              return itensMenu.map((String item){
+                return PopupMenuItem<String>(
+                  value: item,
+                  child: Text(item),
+                );
+              }).toList();
+            },
+          )
+        ],
       ),
       body: Container(),
     );
