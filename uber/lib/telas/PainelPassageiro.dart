4fd8fd55b7527ed3c83a01f0df751ee8e005f25b
Carlos Gabriel <gabrieldfm13@gmail.com>
2020-06-30 22:33:42 -0300

    movimentar e posicionar camera

--- uber/lib/telas/PainelPassageiro.dart
+++ uber/lib/telas/PainelPassageiro.dart
@@ -12,6 +12,8 @@ class PainelPassageiro extends StatefulWidget {
 class _PainelPassageiroState extends State<PainelPassageiro> {
   List<String> itensMenu = ["Deslogar", "Configurações"];
   Completer<GoogleMapController> _controller = Completer();
+  CameraPosition _posicaoCamera = CameraPosition(
+            target: LatLng(-26.0000,-240000));
 
   _escolhaItemMenu(String escolha){
     switch (escolha) {
@@ -31,6 +33,35 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
 
   _onMapCreated(GoogleMapController controller){
     _controller.complete(controller);
+  }
+
+  _recuperarUltimaLocalizacaoConhecida()async{
+    Position position = await Geolocator()
+      .getLastKnownPosition(desiredAccuracy: LocationAccuracy.high);
+
+    setState(() {
+      if(position != null){
+        _posicaoCamera = CameraPosition(
+          target: LatLng(position.latitude, position.longitude),
+          zoom: 19
+        );
+
+        _movimentarCamera(_posicaoCamera);
+      }
+    });
+  }
+
+  _movimentarCamera(CameraPosition cameraPosition)async{
+    GoogleMapController googleMapController = await _controller.future;
+    googleMapController.animateCamera(
+      CameraUpdate.newCameraPosition(cameraPosition)
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _recuperarUltimaLocalizacaoConhecida();
   }
 
   @override
@@ -54,10 +85,7 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
       ),
       body: Container(
         child: GoogleMap(
-          initialCameraPosition: CameraPosition(
-            target: LatLng(-26.0000,-240000),
-            zoom: 16
-          ),
+          initialCameraPosition: _posicaoCamera,
           onMapCreated: _onMapCreated,
           mapType: MapType.normal,
         ),
