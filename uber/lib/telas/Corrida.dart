e231c1ee7a27a89dfad49f43e70af86c2e9671e6
Carlos Gabriel <gabrieldfm13@gmail.com>
2020-07-08 22:30:55 -0300

    criando interface de corrida

---
+++ uber/lib/telas/Corrida.dart
@@ -0,0 +1,93 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:uber/util/StatusRequisicao.dart';
+
+class Corrida extends StatefulWidget {
+  @override
+  _CorridaState createState() => _CorridaState();
+}
+
+class _CorridaState extends State<Corrida> {
+
+  List<String> itensMenu = ["Deslogar", "Configurações"];
+  Completer<GoogleMapController> _controller = Completer();
+  CameraPosition _posicaoCamera =
+      CameraPosition(target: LatLng(-26.0000, -240000));
+  Set<Marker> _marcadores = {};
+
+  _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  _adicionarListenerLocalizacao() {
+    var geolocator = Geolocator();
+    var locationOptions =
+        LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 10);
+
+    geolocator.getPositionStream(locationOptions).listen((Position position) {
+      _exibirMarcadorPassageiro(position);
+
+      _posicaoCamera = CameraPosition(
+          target: LatLng(position.latitude, position.longitude), zoom: 19);
+      _movimentarCamera(_posicaoCamera);
+    });
+  }
+
+  _recuperarUltimaLocalizacaoConhecida() async {
+    Position position = await Geolocator()
+        .getLastKnownPosition(desiredAccuracy: LocationAccuracy.high);
+
+    setState(() {
+      if (position != null) {
+        _exibirMarcadorPassageiro(position);
+        _posicaoCamera = CameraPosition(
+            target: LatLng(position.latitude, position.longitude), zoom: 19);
+
+        _movimentarCamera(_posicaoCamera);
+      }
+    });
+  }
+
+  _movimentarCamera(CameraPosition cameraPosition) async {
+    GoogleMapController googleMapController = await _controller.future;
+    googleMapController
+        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  }
+
+  _exibirMarcadorPassageiro(Position local) async {
+    double pixelRatio = MediaQuery.of(context).devicePixelRatio;
+
+    BitmapDescriptor.fromAssetImage(
+            ImageConfiguration(devicePixelRatio: pixelRatio),
+            "imagens/motorista.png")
+        .then((BitmapDescriptor icone) {
+      Marker marcadorMotorista = Marker(
+          markerId: MarkerId("marcador-motorista"),
+          position: LatLng(local.latitude, local.longitude),
+          infoWindow: InfoWindow(title: "Meu local"),
+          icon: icone);
+      setState(() {
+        _marcadores.add(marcadorMotorista);
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _recuperarUltimaLocalizacaoConhecida();
+    _adicionarListenerLocalizacao();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
