d03809cc4d0c6a993746534178a8bb2847636338
Carlos Gabriel <gabrieldfm13@gmail.com>
2020-07-24 23:35:32 -0300

    animando container

--- animacoes/lib/AnimacaoImplicita.dart
+++ animacoes/lib/AnimacaoImplicita.dart
@@ -6,23 +6,27 @@ class AnimacaoImplicita extends StatefulWidget {
 }
 
 class _AnimacaoImplicitaState extends State<AnimacaoImplicita> {
+
+  bool _status = true;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Container(
+        AnimatedContainer(
           padding: EdgeInsets.all(20),
-          width: 200,
-          height: 200,
-          color: Colors.purpleAccent,
+          width: _status ? 200 : 300,
+          height: _status ? 300 : 200,
+          color: _status ? Colors.white : Colors.purpleAccent,
+          duration: Duration(seconds: 2),
           child: Image.asset("imagens/logo.png"),
         ),
         RaisedButton(
           child: Text("Alterar"),
           onPressed: () {
             setState(() {
-              
+              _status = !_status;
             });
           },
         )
