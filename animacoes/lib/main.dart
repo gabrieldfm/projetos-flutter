e837f5ecd02e125e99c99af8588054400181ef3d
Carlos Gabriel <gabrieldfm13@gmail.com>
2020-07-28 20:16:29 -0300

    animações rotation

--- animacoes/lib/main.dart
+++ animacoes/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:animacoes/AnimacaoImplicita.dart';
 import 'package:animacoes/AnimacaoTween.dart';
+import 'package:animacoes/AnimacoesExplicitasConstruidas.dart';
 import 'package:animacoes/CriandoAnimacoesBasicas.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: AnimacaoTween(),
+    home: AnimacoesExplicitasConstruidas(),
   ));
 }
