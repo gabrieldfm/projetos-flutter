198e96ab60c80e8acfcbc5a33c2469c6093018fc
Carlos Gabriel <gabrieldfm13@gmail.com>
2020-08-01 14:18:58 -0300

    nova animação

--- animacoes/lib/main.dart
+++ animacoes/lib/main.dart
@@ -2,11 +2,12 @@ import 'package:animacoes/AnimacaoImplicita.dart';
 import 'package:animacoes/AnimacaoTween.dart';
 import 'package:animacoes/AnimacoesExplicitasConstruidas.dart';
 import 'package:animacoes/CriandoAnimacoesBasicas.dart';
+import 'package:animacoes/MaisSobreAnimacoes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: AnimacoesExplicitasConstruidas(),
+    home: MaisSobreAnimacoes(),
   ));
 }
