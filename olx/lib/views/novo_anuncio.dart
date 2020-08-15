5139964e7b0f7092a371045c3be2004ed63da309
Carlos Gabriel <gabrieldfm13@gmail.com>
2020-08-15 19:43:29 -0300

    criando pag anuncio

--- olx/lib/views/novo_anuncio.dart
+++ olx/lib/views/novo_anuncio.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:olx/views/widgets/botao_customizado.dart';
 
@@ -7,7 +9,11 @@ class NovoAnuncio extends StatefulWidget {
 }
 
 class _NovoAnuncioState extends State<NovoAnuncio> {
+
+  List<File> _listaImagens = List();
   final _formKey = GlobalKey<FormState>();
+
+  _selecionarImagem(){}
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +29,77 @@ class _NovoAnuncioState extends State<NovoAnuncio> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                FormField(),
+                FormField<List>(
+                  initialValue: _listaImagens,
+                  validator: (imagens){
+                    if (imagens.length == 0) {
+                      return "Necessário selecionar uma imagem";
+                    }
+
+                    return null;
+                  },
+                  builder: (state){
+                    return Column(
+                      children: <Widget>[
+                        Container(
+                          height: 100,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: _listaImagens.length + 1,
+                            itemBuilder: (context, index) {
+                              if (index == _listaImagens.length) {
+                                return Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 8),
+                                  child: GestureDetector(
+                                    onTap: (){
+                                      _selecionarImagem();
+                                    },
+                                    child: CircleAvatar(
+                                      backgroundColor: Colors.grey[400],
+                                      radius: 50,
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: <Widget>[
+                                          Icon(
+                                            Icons.add_a_photo,
+                                            size: 40,
+                                            color: Colors.grey[100],
+                                          ),
+                                          Text(
+                                            "Adicionar",
+                                            style: TextStyle(
+                                              color: Colors.grey[100]
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              }
+
+                              if (_listaImagens.length > 0) {
+                                
+                              }
+
+                              return Container();
+                            },
+                          ),
+                        ),
+                        if(state.hasError)
+                          Container(
+                            child: Text(
+                              "[${state.errorText}]",
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 14
+                              ),
+                            ),
+                          )
+                      ],
+                    );
+                  },
+                ),
                 Row(
                   children: <Widget>[
                     Text("Estado"),
