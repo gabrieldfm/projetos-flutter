c07c6cd15d88e8ddba704a0c6e16b250e6beba19
Carlos Gabriel <gabrieldfm13@gmail.com>
2020-08-23 11:20:01 -0300

    finalizando detlahes anuncion, ligação

--- olx/lib/views/detalhes_anuncio.dart
+++ olx/lib/views/detalhes_anuncio.dart
@@ -2,6 +2,7 @@ import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:olx/main.dart';
 import 'package:olx/models/anuncio.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DetalhesAnuncio extends StatefulWidget {
 
@@ -31,6 +32,12 @@ class _DetalhesAnuncioState extends State<DetalhesAnuncio> {
     }).toList();
   }
 
+  _ligarTelefone(String telefone)async{
+    if (await canLaunch("tel:$telefone")) {
+      await launch("tel:$telefone");
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -57,8 +64,87 @@ class _DetalhesAnuncioState extends State<DetalhesAnuncio> {
                   autoplay: false,
                   dotIncreasedColor: temaPadrao.primaryColor,
                 ),
+              ),
+              Container(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "R\$ ${_anuncio.preco}",
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: temaPadrao.primaryColor
+                      ),
+                    ),
+                    Text(
+                      _anuncio.titulo,
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      child: Divider(),
+                    ),
+                    Text(
+                      "Descrição",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      _anuncio.descricao,
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      child: Divider(),
+                    ),
+                    Text(
+                      "Contato",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 66),
+                      child: Text(
+                        _anuncio.telefone,
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               )
             ],
+          ),
+          Positioned(
+            left: 16,
+            right: 16,
+            bottom: 16,
+            child: GestureDetector(
+              child: Container(
+                child: Text("Ligar", style: TextStyle(color: Colors.white, fontSize: 20),),
+                padding: EdgeInsets.all(16),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: temaPadrao.primaryColor,
+                  borderRadius: BorderRadius.circular(30)
+                ),
+              ),
+              onTap: (){
+                _ligarTelefone(_anuncio.telefone);
+              },
+            ),
           )
         ],
       ),
